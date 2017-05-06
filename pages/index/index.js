eb4181d74ddb269664149f4857df746b492e0142
pbx <pibicha@163.com>
2017-05-06 22:50:55 +0800

    # 首页中部使用WEUI的navbar，实现导航条(ps:代码实现得好麻烦点都不友好，样式还不好看)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,55 @@
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
 
-  data:{
-    popularList:[],
-  },
-  
-  // 伪数据
-  getPopular:function(){
-    var fakePop = new Array(6);
-    for(var i =0;i<6;i++){
-      var ele = new Object();
-      ele.name = "皇家"+i+"号";
-      fakePop[i] = ele;
-    }
-    this.setData({
-      popularList:fakePop,
-    })
-  },
+    data: {
+        // start 导航条需要的变量(weui感觉好不人性化，还不如bootstrap好用)
+        tabs: ["选项一", "选项二", "选项三"],
+        activeIndex: 1,
+        sliderOffset: 0,
+        sliderLeft: 0,
+        // end
 
-  onShow(){
-    this.getPopular();
-  }
+        popularList: [],
+    },
+
+    // 伪数据
+    getPopular: function () {
+        var fakePop = new Array(6);
+        for (var i = 0; i < 6; i++) {
+            var ele = new Object();
+            ele.name = "皇家" + i + "号";
+            fakePop[i] = ele;
+        }
+        this.setData({
+            popularList: fakePop,
+        })
+    },
+
+    //导航相关
+    navbar: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            }
+        });
+    },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    },
+
+
+    onShow() {
+        this.getPopular();
+        navbar;
+
+    }
 
 })
